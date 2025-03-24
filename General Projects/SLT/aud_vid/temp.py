d786a9cad87c27540b3eb3a58ab090ef4a6efcde
Abhinav-Krishnan-Ds <abhinavkrishnan.ds@gmail.com>
2025-03-24 20:25:02 +0530

    T

---
+++ General Projects/SLT/aud_vid/temp.py
@@ -0,0 +1,12 @@
+import nltk
+
+nltk.download("punkt")
+
+# after, again, against, age, all, alone, also, and, ask, at, be, beautiful, before, best, better, busy,
+# but, bye, can, cannot, change, college, come, computer, day, distance, do not, do, does not, eat, engineer,
+# fight, finish, from, glitter, go, god, gold, great, hand, hands, happy, hello, help, her, here, 
+# his, home, homepage, how, invent, it, keep, language, laugh, learn, me, more, my, name, next, not,
+# now, of, on, our, out, pretty, right, sad, safe, self, sign, sing, so, sound, stay, study, talk,
+# television, thank, that, they, this, those, time, to, type, us, walk, wash, way, we, welcome, what,
+# when, where, which, who, whole, whose, why, will, with, without, words, work, world, wrong, you, your,
+# yourself
